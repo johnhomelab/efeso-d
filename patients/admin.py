ea5c5677552671b8bdb72cd90bd65b53b336c105
johnhomelab <john.andrade.fsa@gmail.com>
2026-02-04 23:11:23 -0300

    Atualizar o admin.py

--- patients/admin.py
+++ patients/admin.py
@@ -5,7 +5,7 @@ from .models import Patient
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ("full_name", "clinic", "mobile_phone", "cpf", "created_at")
+    list_display = ("full_name", "clinic", "whatsapp_phone", "cpf", "created_at")
     list_filter = ("clinic", "gender")
-    search_fields = ("full_name", "mobile_phone", "cpf", "email")
+    search_fields = ("full_name", "whatsapp_phone", "cpf", "email")
     ordering = ("full_name",)
