633a5b148e1d15f883fc0c550a1377ad6caa6deb
johnhomelab <john.andrade.fsa@gmail.com>
2026-02-04 22:43:06 -0300

    Atualizar o admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
+from .models import Clinic
 
+
+@admin.register(Clinic)
+class ClinicAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "active", "created_at")
+    list_filter = ("active",)
+    search_fields = ("name", "slug")
+    ordering = ("name",)
