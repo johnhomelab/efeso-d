6bd8c81b1d62c93710338a27cb28631d08b49a78
johnhomelab <john.andrade.fsa@gmail.com>
2026-02-19 07:07:07 -0300

    Atualizar o views.py

--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from patients.models import Patient
 
 def home(request):
-    return render(request, "ui/home.html")
+    # MVP: contar todos os pacientes (mais tarde vamos filtrar por clinic/tenant)
+    patients_count = Patient.objects.count()
+
+    return render(request, "ui/home.html", {
+        "patients_count": patients_count,
+    })
